5227ad84885e4ba9e8098e82c6cfba7b6f4c76fc
prjwl4 <prjwl4@gmail.com>
2021-10-26 23:55:51 +0530

    Add project files.

---
+++ Controllers/VideoController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Azure.Storage.Blobs;
+using System.IO;
+
+namespace VideoStreamFromAzureBlob.Controllers
+{
+
+    [ApiController]
+    public class VideoController : ControllerBase
+    {
+        [HttpGet("api/video")]
+        public async Task<IActionResult> GetVideo()
+        {
+
+            var client = new BlobServiceClient("CONNECTION_STRING");
+
+            var blobClient = client.GetBlobContainerClient("CONTAINER_NAME");
+
+            var blob = blobClient.GetBlobClient("BLOB_NAME");
+
+            Stream str = await blob.OpenReadAsync();
+
+            return new FileStreamResult(str, "video/mp4")
+            {
+                EnableRangeProcessing = true
+            };
+
+        }
+    }
+}
